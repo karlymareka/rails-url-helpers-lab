8974faa412bf06078e05e9d3ef0c8f82744a6231
Karly Mareka <karly.mareka@gmail.com>
2021-09-17 11:57:19 -0400

    save progress

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,30 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+    #binding.pry 
+    if @student.active === true
+      @activity_status = 'active'
+    else
+      @activity_status = 'inactive'
+    end
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active === true
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save!
+    render "show"
   end
 
   private
